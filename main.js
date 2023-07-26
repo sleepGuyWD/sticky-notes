6a79d825e7a4e6fc347042b7d745bbaddb808647
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-07-25 19:16:27 -0700

    localStorage function added

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 import "./style.css";
-import { addNote, removeNote } from "./src/components/note";
+import {
+  addNote,
+  removeNote,
+  saveSessionData,
+  loadSessionData,
+} from "./src/components/note";
 
 window.addEventListener("click", (event) => {
   if (event.target.classList.contains("note-text")) return;
   if (event.target.classList.contains("note-deleter")) {
     removeNote(event.target.parentElement);
+    saveSessionData();
     return;
   }
   addNote(event.pageX, event.pageY);
+  saveSessionData();
 });
 
-//class for notes = "note"
+loadSessionData();
